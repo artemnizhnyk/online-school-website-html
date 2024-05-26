f8df24509e89d5dd1fef7ff4c2892df0658b37e2
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-05-26 22:13:17 +0200

    team section first part was added

--- createx/js/main.js
+++ createx/js/main.js
@@ -1,6 +1,5 @@
 const directionsFilterBtns = document.querySelectorAll(`.directions__filter-btn`);
 
-console.log(directionsFilterBtns);
 directionsFilterBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
         directionsFilterBtns.forEach(item => item.classList.remove("directions__filter-btn--active"));
