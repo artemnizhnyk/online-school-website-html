dc57aabaa9ed3f9cc23c4c9a286b456df88f5a52
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-11 21:59:47 +0200

    program section was added and some fixes in companies section

--- createx/js/main.js
+++ createx/js/main.js
@@ -36,4 +36,17 @@ $(document).ready(() =>{
         e.preventDefault();
         $('.say__slider').slick('slickNext');
     });
+
+    $('.program__acc-link').on('click', function (e) {
+        e.preventDefault()
+        if ($(this).hasClass('program__acc-link--active')) {
+            $(this).removeClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideUp()
+        } else {
+            $('.program__acc-link').removeClass('program__acc-link--active')
+            $('.program__acc-text').slideUp()
+            $(this).addClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideDown()
+        }
+    })
 });
